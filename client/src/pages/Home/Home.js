b9f6c94c115b480cd1430b0ea73e2a97e4ef472c
Hadas <hadas716@gmail.com>
2023-03-04 16:09:12 +0200

    remove series from home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -9,7 +9,6 @@ import Myloader from "react-spinners/PuffLoader";
 
 const Home = () => {
   const [allContent, setAllContent] = useState([]);
-  const [popularSeries, setPopularSeries] = useState([]);
   const [topRated, setTopRated] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   // eslint-disable-next-line
@@ -32,20 +31,6 @@ const Home = () => {
     }
   };
 
-  const fetchPopularSeriesApi = async () => {
-    try {
-      const { data } = await axios.get(` 
-      https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.REACT_APP_API_KEY}&page=1`);
-      const alldata = data.results;
-      const filter = alldata.slice(0, 7);
-      setPopularSeries(filter);
-      setIsLoading(true);
-
-      // eslint-disable-next-line
-    } catch (error) {
-      console.error(error);
-    }
-  };
   const fetchTopRatedApi = async () => {
     try {
       const { data } = await axios.get(` 
@@ -65,11 +50,9 @@ const Home = () => {
     window.scroll(0, 0);
 
     fetchPopularMovieApi();
-    fetchPopularSeriesApi();
     fetchTopRatedApi();
     // eslint-disable-next-line
     return () => {
-      setPopularSeries();
       setAllContent();
       // setTheTrailers();
     };
@@ -101,28 +84,6 @@ const Home = () => {
               {allContent &&
                 allContent.map((n) => (
                   <SingleData key={n.id} {...n} mediaType="movie" />
-                ))}
-            </div>
-          </div>
-          <hr />
-          <div className="TreadingHome3">
-            <div className="title__home">
-              <div className="btn__home">
-                <h6>
-                  TvSeries On Air &#160;
-                  <span style={{ paddingTop: "10px" }}>&#11166;</span>
-                </h6>
-              </div>
-              <div className="view__more">
-                <Link to="/all-series" style={{ textDecoration: "none" }}>
-                  <p>View more &#187;</p>
-                </Link>
-              </div>
-            </div>
-            <div className="ListContent2">
-              {popularSeries &&
-                popularSeries.map((n) => (
-                  <SingleData key={n.id} mediaType="tv" {...n} />
                 ))}
             </div>
           </div>
